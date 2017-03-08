61c8c31dce2bc162f67bdd3aa8d4b4c34a03539f
Chris Wammes <chris.wammes@gmail.com>
2017-03-07 20:12:32 -0500

    Fix parsing issue

--- app/src/main/java/org/techtoledo/dao/EventsDAO.java
+++ app/src/main/java/org/techtoledo/dao/EventsDAO.java
@@ -150,7 +150,7 @@ public class EventsDAO {
                     result.append("LOCATION: TBD\n");
 
                 //Fix encoding issues coming from Calagator
-                line = URLDecoder.decode(line, "ASCII");
+                //line = URLDecoder.decode(line, "ASCII");
                 line = line.replace("&#13\\;", "");
 
                 result.append(line + "\n");
